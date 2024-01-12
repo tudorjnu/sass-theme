975ae77d4d603770bd01bab722b7cf87e78e58d1
Tudor Jianu <tudorjnu@gmail.com>
2024-01-12 15:07:40 +0000

    clean

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
 import "normalize.css";
 import "./styles/index.scss";
-// import "./styles/theme/styles.scss";
-
-import App from "./App";
-
-document.addEventListener("DOMContentLoaded", () => {
-  new App();
-});
